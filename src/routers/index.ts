cab12b137291833afd7e526597ec0a5f6018f686
heart <7362469@qq.com>
2022-11-03 15:56:35 +0800

    chore:init commit

---
+++ src/routers/index.ts
@@ -0,0 +1,20 @@
+import * as VueRouter from 'vue-router'
+import { defineAsyncComponent } from 'vue'
+export const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: defineAsyncComponent(() => import('../pages/home/index.vue'))
+  },
+  // {
+  //   path: '/:pathMatch(.*)',
+  //   name: '404',
+  //   component: defineAsyncComponent(() => import('../pages/404/index.vue'))
+  // }
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes
+})
+export default router
